fd3d530af6ad4dddf0120514a88b3f6eb18fe0ec
rl627 <rl627@drexel.edu>
2021-11-18 11:15:42 -0500

    PN3: Figure 1 y-axis update #8

--- App/appDevTools.R
+++ App/appDevTools.R
@@ -1,16 +1,48 @@
+# Dev -----
+{
+  {
+    library(cowplot)
+    library(GGally)
+    ## min packages
+    library(shiny)
+    library(shinyWidgets)
+    library(waiter)
+    library(dplyr)
+    library(DT)
+    library(reactable)
+    library(sf)
+    library(leaflet)
+    library(plotly)
+    library(rintrojs)
+    library(shinyjs)
+    
+    library(htmlwidgets)
+    ### Load UI Components
+    source("appUI.R", local = T)
+    load("cleaned_le_income_cities_UIelements.rdata")
+  }
+  
+  {
+    load("cleaned_le_income_cities_appBundle.rdata")
+  }
+}
+
 input = list()
 input$fig1_ineq = "Total"
 input$fig1_type = "Abs. Disparity"
 
 
-
+## Total
 outcomeTmp = "Total"
+typeTmp = "Rel. Disparity"
 typeTmp = "Abs. Disparity"
 sizeTmp ="<120,000"
 ineqTmp ="Total"
 typeTmp = "Abs. Disparity"
 
-
+## Income
+outcomeTmp = "Income"
+typeTmp = "Top/Bottom Difference"
 
 # Figure 1 Scr -----
 outcomeTmp = input$fig1_ineq
--- App/appPlotterFigure1.R
+++ App/appPlotterFigure1.R
@@ -6,6 +6,12 @@ figure1_plotter = function(outcomeTmp,typeTmp){
   df_tmp = df_tmp %>% mutate(Region_Name=factor(Region_Name, levels = levelsTmp))
   title_tmp = ifelse(outcomeTmp =="Total","Total Life Expectancy Disparities by MSA","Income-based Life Expectancy Disparities by MSA")
   yaxis_tmp = typeTmp
+  
+  minTmp = ifelse(min(df_tmp$value)<0,min(df_tmp$value)*1.05,min(df_tmp$value)*0.95)
+  yMinTmp = ifelse(typeTmp%in%c('Top/Bottom Ratio','Rel. Disparity'), 1,0)
+  if (minTmp<yMinTmp){yMinTmp=minTmp}
+  yMaxTmp = max(df_tmp$value)*1.01
+  
   plot1 = ggplot(df_tmp,aes(x=Region_Name, y=value))+
     geom_boxplot(aes(group=(Region_Name)), fill=NA, outlier.color = NA, width=0.5)+
     geom_jitter(aes(fill=(Region_Name), size=total_pop, text = tooltip), 
@@ -22,7 +28,13 @@ figure1_plotter = function(outcomeTmp,typeTmp){
           axis.text.x=element_text(size=10, color="black"),
           axis.text.y=element_text(size=10, color="black"),
           axis.title.y=element_text(face="bold", size=12),
-          plot.title=element_text(face="bold", size=13))
+          plot.title=element_text(face="bold", size=13))+
+    ylim(c(yMinTmp,yMaxTmp))
+  
+  ## Add lines
+  if (typeTmp %in% c('Top/Bottom Ratio','Rel. Disparity')){plot1=plot1+ geom_hline(yintercept = 1, lty = 2)}
+  if (typeTmp %in% c('Top/Bottom Difference','Abs. Disparity')){plot1=plot1+ geom_hline(yintercept = 0, lty = 2)}
+  
   ggp = ggplotly(p = plot1,
            tooltip = c("text")) %>% 
     config(displayModeBar = F)
